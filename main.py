2894c4bd3b203f980a1e12e2429aa49697d30939
Franklingamez <88005200+Franklingamez@users.noreply.github.com>
2022-05-25 18:01:52 +1000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+no = 0
+count = 0
 
+def on_button_pressed_a():
+    global no, count
+    no = 1
+    count = randint(10, 100)
+    basic.show_string("NUMBER IS " + ("" + str(count)))
+    while no <= count:
+        basic.show_string("" + str(no))
+        no = no + 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let no = 0
+let count = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    no = 1
+    count = randint(10, 100)
+    basic.showString("NUMBER IS " + ("" + ("" + count)))
+    while (no <= count) {
+        basic.showString("" + ("" + no))
+        no = no + 1
+    }
+})
