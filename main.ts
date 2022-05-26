32209b3d5f5c0c25b54ca7f203c4542d3e115190
Franklingamez <88005200+Franklingamez@users.noreply.github.com>
2022-05-26 12:30:16 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 let no = 0
 let count = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     no = 1
     count = randint(10, 100)
-    basic.showString("NUMBER IS " + ("" + ("" + count)))
+    basic.showString("NUMBER IS " + ("" + count))
     while (no <= count) {
-        basic.showString("" + ("" + no))
+        basic.showString("" + no)
         no = no + 1
     }
 })
